4034000f3a2ad366b7c94b01ac2dc9a0dc2d5501
Nikitha Mannam <nikitha.mannam@inteqsolutions.com>
2025-04-30 16:50:45 +0530

    Added Claim Component

--- src/app/start-claim/start-claim.component.ts
+++ src/app/start-claim/start-claim.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-start-claim',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './start-claim.component.html',
   styleUrl: './start-claim.component.css'
 })
 export class StartClaimComponent {
+  selectedTopic: string = '';
+  isSeekingMedicalAttention: string='';
 
 }
